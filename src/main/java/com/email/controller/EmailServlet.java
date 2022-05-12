4075d25438a892c1ea8be6695d57f59f2826405c
CJN <k3133k@gmail.com>
2022-05-12 18:30:30 +0800

    update Email trashCan & error account letter exception & sweetalert

--- src/main/java/com/email/controller/EmailServlet.java
+++ src/main/java/com/email/controller/EmailServlet.java
@@ -49,8 +49,6 @@ public class EmailServlet extends HttpServlet {
 				/***************************1.接收請求參數****************************************/
 				String replyAccount = req.getParameter("replyAccount");
 				/***************************2.開始查詢資料****************************************/
-//				EmailDetailService emailDetailService = new EmailDetailService();
-//				EmailDetailVO emailDetailVO = emailDetailService.getOneLetter(email_num);
 				/***************************3.查詢完成,準備轉交(Send the Success view)************/
 				req.setAttribute("replyAccount", replyAccount);
 				String url = "/email/sendEmail.jsp???????";
@@ -59,13 +57,12 @@ public class EmailServlet extends HttpServlet {
 
 				/***************************其他可能的錯誤處理**********************************/
 			} catch (Exception e) {
-				errorMsgs.add("無法取得要修改的資料:" + e.getMessage());
+				errorMsgs.add("無法取得修改的資料:");
 				RequestDispatcher failureView = req
 						.getRequestDispatcher("/email/Email.jsp啥");
 				failureView.forward(req, res);
 			}
 		}
-		
 		
 		if ("showletter".equals(action)) { 
 			List<String> errorMsgs = new LinkedList<String>();
@@ -86,26 +83,25 @@ public class EmailServlet extends HttpServlet {
 
 				/***************************其他可能的錯誤處理**********************************/
 			} catch (Exception e) {
-				errorMsgs.add("無法取得要修改的資料:" + e.getMessage());
+				errorMsgs.add("無法取得修改的資料:");
 				RequestDispatcher failureView = req
 						.getRequestDispatcher("/email/Email.jsp啥");
 				failureView.forward(req, res);
 			}
 		}
 
-
-        if ("insert".equals(action)) { // 來自addEmp.jsp的請求  
-			
+        if ("insert".equals(action)) { 
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
 			EmailDetailService emailDetailService = new EmailDetailService();
+			String mem_account =null;//catch會用到因此拉到外面來
 			
 			try {
 				/***********************1.接收請求參數 - 輸入格式的錯誤處理*************************/
-				String mem_account = req.getParameter("mem_account");//收件人
-				Integer mem_access = emailDetailService.findMemAccess(mem_account);//1=廠商,2=網紅
+				mem_account = req.getParameter("mem_account");//收件人
+				Integer mem_access = emailDetailService.findMemAccess(mem_account);//判斷收件者的類型1=廠商,2=網紅
 				if (mem_access == null) {
-					errorMsgs.add("帳號輸入錯誤,沒有此會員帳號");
+					throw new EmailAccountException();//當無此收件者時拋出自訂例外
 				}
 				if (mem_account == null || mem_account.trim().length() == 0) {
 					errorMsgs.add("收件人: 請勿空白");
@@ -114,16 +110,14 @@ public class EmailServlet extends HttpServlet {
 				if (email_content == null || email_content.trim().length() == 0 || "<p><br></p>".equals(email_content)) {
 					errorMsgs.add("信件內容: 請勿空白");
 				}
-				
-				Integer email_typenum = 1;//信件類別,先寫死
-				
+				Integer email_typenum = 1;//信件類別,一般寄信為普通信件=1
 				String email_title = req.getParameter("email_title");
 				if (email_title == null || email_title.trim().length() == 0) {
 					errorMsgs.add("信件標題: 請勿空白");
 				}
 				
 				EmailDetailVO emailDetailVO =new EmailDetailVO();
-				if(mem_access==1) {//1=廠商,2=網紅
+				if(mem_access==1) {//根據收件者類型新增1=廠商,2=網紅
 				emailDetailVO.setCom_account(mem_account);
 				}else if(mem_access==2) {
 				emailDetailVO.setKol_account(mem_account);
@@ -132,12 +126,8 @@ public class EmailServlet extends HttpServlet {
 				emailDetailVO.setEmail_title(email_title);
 				emailDetailVO.setEmail_content(email_content);
 				emailDetailVO.setSender(loginVo.getMebAccount());//寄件人
-//				emailDetailVO.setSender("tibameKOL");//寄件人登入還沒做好先寫死
-				
 				
 				if (!errorMsgs.isEmpty()) {
-//					req.setAttribute("productVO", productVO);
-//					fileContent.close();
 					RequestDispatcher failureView = req
 							.getRequestDispatcher("/email/sendEmail.jsp");
 					failureView.forward(req, res);
@@ -147,23 +137,26 @@ public class EmailServlet extends HttpServlet {
 				/***************************2.開始新增資料***************************************/
 				emailDetailService.send(emailDetailVO);
 				/***************************3.新增完成,準備轉交(Send the Success view)***********/
-//				fileContent.close();
-//				req.setAttribute("productVO", productVO);
 				String url = "/email/Email.jsp";
-				RequestDispatcher successView = req.getRequestDispatcher(url); // 新增成功後轉交listAllEmp.jsp
+				RequestDispatcher successView = req.getRequestDispatcher(url); 
 				successView.forward(req, res);				
 				
 				/***************************其他可能的錯誤處理**********************************/
+			} catch (EmailAccountException e) {//當無此收件者。管理員寄一封計件失敗信件
+				//傳入三個參數:寄件失敗者的帳號,寄件者會員權限,輸入錯誤的帳號
+				emailDetailService.sendErrorLetter(loginVo.getMebAccount(),loginVo.getMebAccess(),mem_account);
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/email/Email.jsp");
+				failureView.forward(req, res);
 			} catch (Exception e) {
 				errorMsgs.add(e.getMessage());
-//				fileContent.close();
 				RequestDispatcher failureView = req
 						.getRequestDispatcher("/email/sendEmail.jsp");
 				failureView.forward(req, res);
 			}
 		}
         
-		if ("delete".equals(action)) { // 來自listAllEmp.jsp的請求
+		if ("delete".equals(action)) { 
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
 			
@@ -177,6 +170,36 @@ public class EmailServlet extends HttpServlet {
 				for(int i=0; i<email_nums.length; i++) {
 					email_num = new Integer(email_nums[i]);
 					emailDetailService.deleteLetter(email_num);
+				}
+				
+				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+				String url = "/email/trashCan.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得要修改的資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/email/trashCan.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		if ("toTrashCan".equals(action)) { 
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				String[] email_nums =req.getParameterValues("deletePk");
+				
+				/***************************2.開始查詢資料****************************************/
+				EmailDetailService emailDetailService = new EmailDetailService();
+				Integer email_num;
+				for(int i=0; i<email_nums.length; i++) {
+					email_num = new Integer(email_nums[i]);
+					emailDetailService.trashCanLetter(email_num);
 				}
 				
 				/***************************3.查詢完成,準備轉交(Send the Success view)************/
--- src/main/java/com/emailDetail/model/EmailDetailDAO_interface.java
+++ src/main/java/com/emailDetail/model/EmailDetailDAO_interface.java
@@ -8,9 +8,12 @@ public interface EmailDetailDAO_interface {
 	public void insert(EmailDetailVO emailDetailVO);
 	public EmailDetailVO findByPrimaryKey(Integer email_num);//用mailPK找出VO(一封信)
 	public List<EmailDetailVO> findMailBox(String mem_account);//用帳號找出信箱整理出信箱
+	public List<EmailDetailVO> findTrashCanBox(String mem_account);//用帳號找出信箱整理出信箱
 	public void delete(Integer email_num);
+	public void toTrashCan(Integer email_num);
 	public List<EmailDetailVO> getAll();
 //	public void update(EmailDetailVO emailDetailVO);
 	public Integer findAccess(String memAccount);//用會員account找會員權限
+	public void errorLetter(String memAccount,Integer memAccess, String wrongAccount);//用會員account找會員權限
 
 }
