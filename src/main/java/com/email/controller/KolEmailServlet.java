2282e697017f1f9e2b65d32b1642724f6c7dc107
CJN <k3133k@gmail.com>
2022-05-20 20:50:30 +0800

    insert kol email

---
+++ src/main/java/com/email/controller/KolEmailServlet.java
@@ -0,0 +1,324 @@
+package com.email.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import com.emailDetail.model.EmailDetailService;
+import com.emailDetail.model.EmailDetailVO;
+import com.kolmeb.model.KolMebVO;
+import com.mysql.cj.Session;
+import com.product.model.*;
+import com.productType.model.ProductTypeService;
+
+
+@MultipartConfig()
+@WebServlet("/email/kolEmail.do")
+public class KolEmailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		KolMebVO kolMebVO =(KolMebVO)req.getSession().getAttribute("kolMebVO");//登入者的資料
+		
+		if ("rollBack".equals(action)) { // 回信
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				String[] email_nums =req.getParameterValues("rollBackPk");
+				String[] email_types =req.getParameterValues("rollBackType");
+				
+				/***************************2.開始查詢資料****************************************/
+				EmailDetailService emailDetailService = new EmailDetailService();
+				Integer email_num;
+				String email_type = null;
+				for(int i=0; i<email_nums.length; i++) {
+					email_num = new Integer(email_nums[i]);
+					email_type = email_types[i];
+//					System.out.println("email_num="+email_num+"|"+"email_type="+email_type);
+					emailDetailService.rollBackLetter(email_num, email_type);
+				}
+				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+				String url = "/kolBackStage/email/trashCan.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得要修改的資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/kolBackStage/email/trashCan.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		if ("reply".equals(action)) { // 回信
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				String replyAccount = req.getParameter("replyAccount");
+				/***************************2.開始查詢資料****************************************/
+				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+				req.setAttribute("replyAccount", replyAccount);
+				String url = "/kolBackStage/email/sendEmail.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得修改的資料:");
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/kolBackStage/email/Email.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		if ("draftSend".equals(action)) { // 回信
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				Integer email_num = new Integer(req.getParameter("email_num"));
+				/***************************2.開始查詢資料****************************************/
+				EmailDetailService emailDetailService = new EmailDetailService();
+				EmailDetailVO emailDetailVO = emailDetailService.getOneLetter(email_num);
+				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+				req.setAttribute("emailDetailVO", emailDetailVO);
+				String url = "/kolBackStage/email/sendEmail.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得修改的資料:");
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/kolBackStage/email/Email.jsp啥");
+				failureView.forward(req, res);
+			}
+		}
+		
+		if ("showletter".equals(action)) { 
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				Integer email_num = new Integer(req.getParameter("email_num"));
+				
+				/***************************2.開始查詢資料****************************************/
+				EmailDetailService emailDetailService = new EmailDetailService();
+				EmailDetailVO emailDetailVO = emailDetailService.getOneLetter(email_num);
+				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+				req.setAttribute("emailDetailVO", emailDetailVO);
+				String url = "/kolBackStage/email/letter.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得修改的資料:");
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/kolBackStage/email/Email.jsp啥");
+				failureView.forward(req, res);
+			}
+		}
+
+        if ("insert".equals(action)) { 
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			EmailDetailService emailDetailService = new EmailDetailService();
+			String mem_account =null;//catch會用到因此拉到外面來
+			
+			try {
+				/***********************1.接收請求參數 - 輸入格式的錯誤處理*************************/
+				mem_account = req.getParameter("mem_account");//收件人
+				Integer mem_access = emailDetailService.findMemAccess(mem_account);//判斷收件者的類型1=廠商,2=網紅
+				if (mem_account == null || mem_account.trim().length() == 0) {
+					errorMsgs.add("收件人: 請勿空白");
+				}else if(mem_access == null) {
+					throw new EmailAccountException();//當無此收件者時拋出自訂例外
+				}
+				String email_content = req.getParameter("email_content");
+				if (email_content == null || email_content.trim().length() == 0 || "<p><br></p>".equals(email_content)) {
+					errorMsgs.add("信件內容: 請勿空白");
+				}
+				Integer email_typenum = 1;//信件類別,一般寄信為普通信件=1
+				String email_title = req.getParameter("email_title");
+				if (email_title == null || email_title.trim().length() == 0) {
+					errorMsgs.add("信件標題: 請勿空白");
+				}
+				
+				EmailDetailVO emailDetailVO =new EmailDetailVO();
+				if(mem_access==1) {//根據收件者類型新增1=廠商,2=網紅
+				emailDetailVO.setCom_account(mem_account);
+				}else if(mem_access==2) {
+				emailDetailVO.setKol_account(mem_account);
+				}
+				emailDetailVO.setEmail_typenum(email_typenum);
+				emailDetailVO.setEmail_title(email_title);
+				emailDetailVO.setEmail_content(email_content);
+				emailDetailVO.setSender(kolMebVO.getKol_account());//寄件人
+				
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/kolBackStage/email/sendEmail.jsp");
+					failureView.forward(req, res);
+					return;
+				}
+				
+				/***************************2.開始新增資料***************************************/
+				emailDetailService.send(emailDetailVO);
+				/***************************3.新增完成,準備轉交(Send the Success view)***********/
+				String url = "/kolBackStage/email/Email.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); 
+				successView.forward(req, res);				
+				
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (EmailAccountException e) {//當無此收件者。管理員寄一封寄件失敗信件
+				//傳入三個參數:寄件失敗者的帳號,寄件者會員權限,輸入錯誤的帳號
+				emailDetailService.sendErrorLetter(kolMebVO.getKol_account(),kolMebVO.getMeb_accessnum(),mem_account);
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/kolBackStage/email/Email.jsp");
+				failureView.forward(req, res);
+			} catch (Exception e) {
+				errorMsgs.add(e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/kolBackStage/email/sendEmail.jsp");
+				failureView.forward(req, res);
+			}
+		}
+        
+        if ("draft".equals(action)) { 
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			EmailDetailService emailDetailService = new EmailDetailService();
+			
+			try {
+				/***********************1.接收請求參數 - 只是加入草稿因此沒有太多資料驗證*************************/
+				/***********************如果使用者存入全部空白的草稿會在前端進行錯誤處理**************************/
+				String mem_account = kolMebVO.getKol_account();//收件人,草稿信收件人為自己
+				Integer mem_access = emailDetailService.findMemAccess(mem_account);//判斷收件者的類型1=廠商,2=網紅
+				String email_content = req.getParameter("draftContent");
+				Integer email_typenum = 2;//信件類別,加入草稿為草稿信件=2
+				String email_title = req.getParameter("draftTitle");
+				if(email_title.equals("")) {
+					email_title ="未命名草稿";
+				}
+				EmailDetailVO emailDetailVO =new EmailDetailVO();
+				if(mem_access==1) {//根據收件者類型新增1=廠商,2=網紅
+				emailDetailVO.setCom_account(mem_account);
+				}else if(mem_access==2) {
+				emailDetailVO.setKol_account(mem_account);
+				}
+				emailDetailVO.setEmail_typenum(email_typenum);
+				emailDetailVO.setEmail_title(email_title);
+				emailDetailVO.setEmail_content(email_content);
+				
+				/***************************2.開始新增資料***************************************/
+				emailDetailService.addDraft(emailDetailVO);
+				/***************************3.新增完成,準備轉交(Send the Success view)***********/
+				String url = "/kolBackStage/email/Email.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); 
+				successView.forward(req, res);				
+				
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add(e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/kolBackStage/email/sendEmail.jsp");
+				failureView.forward(req, res);
+			}
+		}
+        
+		if ("delete".equals(action)) { 
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				String[] email_nums =req.getParameterValues("deletePk");
+				
+				/***************************2.開始查詢資料****************************************/
+				EmailDetailService emailDetailService = new EmailDetailService();
+				Integer email_num;
+				for(int i=0; i<email_nums.length; i++) {
+					email_num = new Integer(email_nums[i]);
+					emailDetailService.deleteLetter(email_num);
+				}
+				
+				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+				String url = "/kolBackStage/email/trashCan.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得要修改的資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/kolBackStage/email/trashCan.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		if ("toTrashCan".equals(action)) { 
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				String[] email_nums =req.getParameterValues("deletePk");
+				
+				/***************************2.開始查詢資料****************************************/
+				EmailDetailService emailDetailService = new EmailDetailService();
+				Integer email_num;
+				for(int i=0; i<email_nums.length; i++) {
+					email_num = new Integer(email_nums[i]);
+					emailDetailService.trashCanLetter(email_num);
+				}
+				
+				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+				String url = "/kolBackStage/email/Email.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得要修改的資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/kolBackStage/email/Email.jsp");
+				failureView.forward(req, res);
+			}
+		}
+        
+        
+        
+        
+        
+        
+	}
+}
---
+++ src/main/webapp/kolBackStage/email/Email.jsp
@@ -0,0 +1,109 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@page import="com.login.*"%>
+<%@ page import="com.emailDetail.model.*"%>
+<%@ page import="java.util.*"%>
+
+<%@ include file="header.jsp"%>
+<%
+EmailDetailService emailDetailService = new EmailDetailService();
+// KolMebVO kolMebVO = (KolMebVO) session.getAttribute("kolMebVO");//讀取登入者的資料 這段有寫在header.jsp上
+List<EmailDetailVO> list = emailDetailService.findMailBox(kolMebVO.getKol_account());
+pageContext.setAttribute("list", list);
+%>
+
+<div class="content-wrapper">
+	<section class="content">
+		<div class="row">
+			<div class="col-sm-2"></div>
+			<div class="col-sm-8">
+				<div class="container h2 jntest" style="text-align: center;">收件夾</div>
+			</div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-1"></div>
+			<!--==縮排用間隔==-->
+			<div class="col-md-9">
+				<div class="card card-primary card-outline">
+					<div class="card-header">
+						<div class="row m-2">
+							<i class="fa fa-envelope-open-o" aria-hidden="true"
+								style="font-size: 22px;"> <span style="color:orange">${kolMebVO.kol_account}</span> 的收件夾</i>
+						</div>
+					</div>
+					<div class="card-body p-0">
+						<div class="mailbox-controls">
+							<div class="row align-items-center">
+								<label class="btn btn-default btn-lg checkbox-toggle"
+									style="display: inline; padding: 8px 12px; height: 38px; width: 48px; margin: 0 15px;">
+									<input type="checkbox" class="checkbox-master "
+									style="width: 20px; height: 20px;">
+								</label>
+								<form action="<%=request.getContextPath()%>/email/kolEmail.do"
+									class="trashCanForm" style="margin: 0;" >
+									<button class="btn btn-default btn-lg trashCanBtn"
+										type="button" style="display: inline;">
+										<i class="far fa-trash-alt"></i>
+									</button>
+									<input type="hidden" name="action" value="toTrashCan">
+								</form>
+							</div>
+						</div>
+
+						<div class="table-responsive mailbox-messages">
+							<table class="table table-hover table-striped mailtable">
+								<thead>
+									<tr>
+										<td></td>
+										<td class="star"></td>
+										<td class="name"><b>寄件者</b></td>
+										<td class="subject"><b>信件標題</b></td>
+										<td class="attachment"></td>
+										<td class="date"><b>日期</b></td>
+									</tr>
+								</thead>
+								<tbody>
+									<c:forEach var="emailDetailVO" items="${list}">
+										<tr>
+											<td>
+												<div class="icheck-primary">
+													<input type="checkbox" value="" id="check1"
+														class="item checkdelete" name="checkdelete"
+														style="width: 20px; height: 20px;"> <label
+														for="check1"></label>
+												</div>
+											</td>
+											<td></td>
+											<td class="mailbox-name">
+												<form action="<%=request.getContextPath()%>/email/kolEmail.do">
+													<button type="submit" class="btn btn-link btn-sm">${emailDetailVO.sender}</button>
+													<input type="hidden" name="action" value="reply"> <input
+														type="hidden" name="replyAccount"
+														value="${emailDetailVO.sender}">
+												</form></td>
+											<td class="mailbox-subject">
+												<form action="<%=request.getContextPath()%>/email/kolEmail.do">
+													<input type="hidden" name="action" value="showletter">
+													<input type="hidden" name="email_num" class="email_num"
+														value="${emailDetailVO.email_num}">
+													<button type="submit" class="btn btn-link btn-sm">
+														<b style="${(emailDetailVO.sender=='ADM')?'background:rgb(233, 191, 191)':''}">${emailDetailVO.email_title}</b>
+													</button>
+
+												</form>
+											</td>
+											<td class="mailbox-attachment"></td>
+											<td class="mailbox-date"><fmt:formatDate
+													value="${emailDetailVO.email_date}"
+													pattern="yyyy-MM-dd HH:mm" /></td>
+										</tr>
+									</c:forEach>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+			<%@ include file="footer.jsp"%>
